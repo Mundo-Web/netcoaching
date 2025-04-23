7cbf246aaaf55c5b1805540b5d7403661250ca73
Manuel Gamboa <gamboapalominocarlosmanuel@gmail.com>
2025-04-23 05:48:34 -0500

    NTS

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
             EventSeeder::class,
             FaqSeeder::class,
             RecoveryEmailSeeder::class,
+            WatchRequestsEmailSeeder::class,
         ]);
     }
 }
