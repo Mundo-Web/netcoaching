9cf56f547c7b8ddd58a055f5beae0ba966d4e9f8
Manuel Gamboa <gamboapalominocarlosmanuel@gmail.com>
2026-01-09 14:28:26 -0500

    NTS

--- app/Http/Controllers/Admin/CoachController.php
+++ app/Http/Controllers/Admin/CoachController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use SoDe\Extend\File;
 use SoDe\Extend\JSON;
+use SoDe\Extend\Text;
 
 class CoachController extends BasicController
 {
@@ -32,5 +33,14 @@ class CoachController extends BasicController
             ->withCount(['resources'])
             ->join('model_has_roles AS mhr', 'mhr.model_id', 'users.id')
             ->where('mhr.role_id', 2);
+    }
+
+    public function beforeSave(Request $request)
+    {
+        $body = $request->all();
+        if ($body['video'] && Text::startsWith($body['video'], 'https://')) {
+            $body['video'] = Text::getYTVideoId($body['video']);
+        }
+        return $body;
     }
 }
